a090810f8ceaf0223b7089c0bece7a3b95052ee2
1018715564 <1018715564@qq.com>
2020-11-29 17:57:25 +0800

    [feate]检查token，用户登录/登出

--- cloudfunctions-aliyun/user/index.js
+++ cloudfunctions-aliyun/user/index.js
@@ -6,13 +6,10 @@ const {
 	appErrorMessage,
 	handleMustRequireParam
 } = require("app-tools");
-const functionSet = {
-	addUserByPhone
-}
+
 
 // 注册用户根据手机号
 function addUserByPhone(event, context) {
-	// 判断是否传入必填参数
 	return handleMustRequireParam([{
 		key: "username",
 		value: "用户名"
@@ -30,17 +27,69 @@ function addUserByPhone(event, context) {
 			return appErrorMessage("密码格式不正确");
 		} else {
 			// 校验手机号
-			const res = await uniID.register({
+			return await uniID.register({
 				username,
 				password
 			});
-			return res;
 		}
 	}).catch(err => {
 		return err;
 	});
 }
 
+// 用户登录根据手机号
+function getLoginByPhone(event, context) {
+	return handleMustRequireParam([{
+		key: "username",
+		value: "用户名"
+	}, {
+		key: "password",
+		value: "密码"
+	}], event).then(async () => {
+		const {
+			username,
+			password
+		} = event;
+		if (!(/^1[3456789]\d{9}$/.test(username))) {
+			return appErrorMessage("用户名格式不正确")
+		} else if (password === "" || password.length < 6) {
+			return appErrorMessage("密码格式不正确");
+		} else {
+			// 校验手机号
+			return await uniID.login({
+				username,
+				password
+			});
+		}
+	}).catch(err => {
+		return err;
+	});
+}
+
+// 用户登出, 登出需要客户端删除持久性缓存
+async function userLogout(event, context) {
+	if(!event.hasOwnProperty("token")){
+		return appErrorMessage("token为必填");
+	}else {
+		return await uniID.logout(event.token);
+	}
+}
+
+// 校验Token是否有效
+async function checkToken(event, context){
+	if(!event.hasOwnProperty("token")){
+		return appErrorMessage("token为必填");
+	}else {
+		return await uniID.checkToken(event.token);
+	}
+}
+
+const functionSet = {
+	addUserByPhone,
+	getLoginByPhone,
+	userLogout,
+	checkToken
+}
 
 exports.main = async (event, context) => {
 	const method = event.$method;
