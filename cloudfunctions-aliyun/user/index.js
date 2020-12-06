a91515ad120f4c7e5b6c61e34bf018430f490a05
1018715564 <1018715564@qq.com>
2020-12-06 16:49:59 +0800

    [feat]用户模块添加修改用户信息接口([语雀接口文档](https://www.yuque.com/docs/share/e2ab630f-002a-4265-b501-85cc1d96d70e?# 《用户模块》))

--- cloudfunctions-aliyun/user/index.js
+++ cloudfunctions-aliyun/user/index.js
@@ -1,100 +1,136 @@
-'use strict';
+"use strict";
 // 公告模块
-const uniID = require('uni-id');
+const uniID = require("uni-id");
 // 工具函数
 const {
 	appErrorMessage,
 	handleMustRequireParam
 } = require("app-tools");
 
-
 // 注册用户根据手机号
 function addUserByPhone(event, context) {
-	return handleMustRequireParam([{
-		key: "username",
-		value: "用户名"
-	}, {
-		key: "password",
-		value: "密码"
-	}], event).then(async () => {
-		const {
-			username,
-			password
-		} = event;
-		if (!(/^1[3456789]\d{9}$/.test(username))) {
-			return appErrorMessage("用户名格式不正确")
-		} else if (password === "" || password.length < 6) {
-			return appErrorMessage("密码格式不正确");
-		} else {
-			// 校验手机号
-			return await uniID.register({
+	return handleMustRequireParam(
+			[{
+					key: "username",
+					value: "用户名",
+				},
+				{
+					key: "password",
+					value: "密码",
+				},
+			],
+			event
+		)
+		.then(async () => {
+			const {
 				username,
 				password
-			});
-		}
-	}).catch(err => {
-		return err;
-	});
+			} = event;
+			if (!/^1[3456789]\d{9}$/.test(username)) {
+				return appErrorMessage("用户名格式不正确");
+			} else if (password === "" || password.length < 6) {
+				return appErrorMessage("密码格式不正确");
+			} else {
+				// 校验手机号
+				return await uniID.register({
+					username,
+					password,
+				});
+			}
+		})
+		.catch((err) => {
+			return err;
+		});
 }
 
 // 用户登录根据手机号
 function getLoginByPhone(event, context) {
-	return handleMustRequireParam([{
-		key: "username",
-		value: "用户名"
-	}, {
-		key: "password",
-		value: "密码"
-	}], event).then(async () => {
-		const {
-			username,
-			password
-		} = event;
-		if (!(/^1[3456789]\d{9}$/.test(username))) {
-			return appErrorMessage("用户名格式不正确")
-		} else if (password === "" || password.length < 6) {
-			return appErrorMessage("密码格式不正确");
-		} else {
-			// 校验手机号
-			return await uniID.login({
+	return handleMustRequireParam(
+			[{
+					key: "username",
+					value: "用户名",
+				},
+				{
+					key: "password",
+					value: "密码",
+				},
+			],
+			event
+		)
+		.then(async () => {
+			const {
 				username,
 				password
-			});
-		}
-	}).catch(err => {
-		return err;
-	});
+			} = event;
+			if (!/^1[3456789]\d{9}$/.test(username)) {
+				return appErrorMessage("用户名格式不正确");
+			} else if (password === "" || password.length < 6) {
+				return appErrorMessage("密码格式不正确");
+			} else {
+				// 校验手机号
+				return await uniID.login({
+					username,
+					password,
+				});
+			}
+		})
+		.catch((err) => {
+			return err;
+		});
 }
 
 // 用户登出, 登出需要客户端删除持久性缓存
 async function userLogout(event, context) {
-	if(!event.hasOwnProperty("token")){
+	if (!event.hasOwnProperty("token")) {
 		return appErrorMessage("token为必填");
-	}else {
+	} else {
 		return await uniID.logout(event.token);
 	}
 }
 
 // 校验Token是否有效
-async function checkToken(event, context){
-	if(!event.hasOwnProperty("token")){
+async function checkToken(event, context) {
+	if (!event.hasOwnProperty("token")) {
 		return appErrorMessage("token为必填");
-	}else {
+	} else {
 		return await uniID.checkToken(event.token);
 	}
+}
+
+// 更新用户信息
+async function updateUserInfo(event, context) {
+	// 更改昵称，性别，头像等信息
+	if (!event.hasOwnProperty("uid")) {
+		return appErrorMessage("用户ID为必填");
+	}
+	// 确定字段是否存在于限定字段之内
+	const specifiedParam = ["nickname", "gender", "avatar"];
+	// 查看event传入的参数，除了uid，有没有传入其他的参数
+	for (let key in event) {
+		if (key !== "uid") {
+			let resultIndex = specifiedParam.indexOf(key);
+			if (resultIndex < 0) {
+				return appErrorMessage(`${key}参数不合法`);
+			}
+		}
+	}
+	return await uniID.updateUser({
+		...event,
+	});
 }
 
 const functionSet = {
 	addUserByPhone,
 	getLoginByPhone,
 	userLogout,
-	checkToken
-}
+	checkToken,
+	updateUserInfo,
+};
 
 exports.main = async (event, context) => {
 	const method = event.$method;
 	if (!functionSet.hasOwnProperty(method)) {
-		return appErrorMessage(`方法名不存在`)
+		return appErrorMessage(`方法名不存在`);
 	}
 	//event为客户端上传的参数
 	// 删除event中的$method
