297a7b09e581167091983045e47d4478d3b155a5
1018715564 <1018715564@qq.com>
2020-12-13 15:06:03 +0800

    [feat]首页静态页面

---
+++ cloudfunctions-aliyun/application/services/explanation.ts
@@ -0,0 +1,8 @@
+namespace Explanation{
+    const explain = require("explain");
+    // 公告模块
+    const uniID = require("uni-id");
+    // 工具函数
+    const { appErrorMessage, handleMustRequireParam } = require("app-tools");
+    module.exports = class Explanation extends explain.service {};
+}
