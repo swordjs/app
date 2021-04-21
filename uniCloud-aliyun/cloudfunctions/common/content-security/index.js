81f127bddba27e9cc0f5a70c3cd863b2d1ffb4d2
1018715564 <1018715564@qq.com>
2021-04-22 07:39:49 +0800

    [fix]新增内容安全模块依赖缓存模块

--- uniCloud-aliyun/cloudfunctions/common/content-security/index.js
+++ uniCloud-aliyun/cloudfunctions/common/content-security/index.js
@@ -1,5 +1,8 @@
+const explain = require("explain");
+explain.cache = require("explain-cache");
+
 module.exports = function(e) {
-	// 公用模块用法请参考 https://uniapp.dcloud.io/uniCloud/cf-common
 	
 	return e
 }
+
