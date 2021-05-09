e51476097c7eacfcbd20259790b217c968910195
1018715564 <1018715564@qq.com>
2021-05-09 20:05:06 +0800

    [feat]钉钉通知配置更改

--- uniCloud-aliyun/cloudfunctions/dingtalk-robot/index.js
+++ uniCloud-aliyun/cloudfunctions/dingtalk-robot/index.js
@@ -1,16 +1,18 @@
 'use strict';
 const ChatBot = require('dingtalk-robot-sender');
+const config = require("./config.json");
 exports.main = (event, context) => {
 	return new Promise(async resolve => {
 		//event为客户端上传的参数
 		const robot = new ChatBot({
-			webhook: "https://oapi.dingtalk.com/robot/send?access_token=099cc8980106a0b6981c6cf58cf91de268563c599a6ee9094b36d94f3eb0c636",
+			webhook: `https://oapi.dingtalk.com/robot/send?access_token=${config.access_token}`,
 		});
 		let textContent = {
 			"msgtype": "text",
 			"text": {
 				"content": event.content
-			}
+			},
+			"at": config.at
 		}
 		robot.send(textContent)
 			.then((res) => {
