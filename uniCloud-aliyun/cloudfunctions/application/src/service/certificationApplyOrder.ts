74a20da6e08b8555e3e97ac29006eb48fa787ca8
seho <1018715564@qq.com>
2021-06-03 07:17:13 +0800

    [feat]新增获取认证列表的api

--- uniCloud-aliyun/cloudfunctions/application/src/service/certificationApplyOrder.ts
+++ uniCloud-aliyun/cloudfunctions/application/src/service/certificationApplyOrder.ts
@@ -21,11 +21,41 @@ namespace CertificationApplyOrderService {
       this.userID = data.context.userID;
       this.nowDate = new Date().toISOString();
     }
+    async getCertificationApplyOrder(params: {
+      limit: number;
+      page: number;
+      state: number;
+    }) {
+      const { limit = 10, page = 1, state } = params;
+      const whereParams = {
+        state,
+      };
+      const data = await collection
+        .where(whereParams)
+        .skip(limit * (page - 1))
+        .limit(limit)
+        .sort({
+          createTime: -1,
+        })
+        .filed(`state, userID, content, createTime`)
+        .get();
+      const countResult = await collection.where(whereParams).count();
+      return {
+        list: data.data,
+        count: countResult.total,
+      };
+    }
     async addCertificationApplyOrder(params: IAddCertificationParams) {
       // 调用钉钉通知函数
       uniCloud.callFunction({
         name: "dingtalk-robot",
-        data: { content: `有一个新的出题官认证申请，申请时间为${this.nowDate}\n申请内容为: ${JSON.stringify(params.content)}\n请及时查看，尤其是群主，你别忘记了！` },
+        data: {
+          content: `有一个新的出题官认证申请，申请时间为${
+            this.nowDate
+          }\n申请内容为: ${JSON.stringify(
+            params.content
+          )}\n请及时查看，尤其是群主，你别忘记了！`,
+        },
       });
       return await collection.add({
         state: "pending",
