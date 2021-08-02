47554074c7471c6e5063298d65c5d8a39c300570
seho <1018715564@qq.com>
2021-08-03 07:00:38 +0800

    [feat]新增公共api的云函数

---
+++ uniCloud-aliyun/cloudfunctions/openApi/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const url = "https://c7e81452-9d28-4486-bedc-5dbf7c8386a5.bspapp.com/http/v1/application";
+
+exports.main = async (event, context) => {
+	// 规定函数的参数，首先获取参数中的用户id和其他信息
+	
+	//返回数据给客户端
+	return event
+};
