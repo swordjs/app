83008c0b8379b6a73ad7a64b6a3a9bfbbddf9832
seho <1018715564@qq.com>
2021-08-14 17:08:37 +0800

    [feat]重置调用次数的云函数

---
+++ uniCloud-aliyun/cloudfunctions/openApi-reset-request-number/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const db = uniCloud.database();
+const collection = db.collection('uni-id-users');
+// 每个月重置的次数
+const RESET_NUMBER = 200;
+
+exports.main = async (event, context) => {
+	// 重置
+	return await collection.where({
+		mobile_confirmed: 1
+	}).update({
+		openApiRequestNumber: RESET_NUMBER
+	})
+};
