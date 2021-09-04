a7419a6cc187340d266e08227642da4d4a2b7f00
seho <1018715564@qq.com>
2021-09-04 23:00:34 +0800

    🐳 chore: 修改ts配置

--- uniCloud-aliyun/cloudfunctions/application/etsc.config.js
+++ uniCloud-aliyun/cloudfunctions/application/etsc.config.js
@@ -1,11 +1,5 @@
 module.exports = {
-  outDir: "./dist",
   esbuild: {
     minify: true,
-    target: "es2015",
-  },
-  assets: {
-    baseDir: "src",
-    filePatterns: ["**/*.json"],
-  },
+  }
 };
