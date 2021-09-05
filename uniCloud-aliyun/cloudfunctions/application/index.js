343e60fccef40621a8bf29bacc65edf81596916a
seho <1018715564@qq.com>
2021-09-05 15:55:03 +0800

    🐞 fix: 添加云函数路由

--- uniCloud-aliyun/cloudfunctions/application/index.js
+++ uniCloud-aliyun/cloudfunctions/application/index.js
@@ -1,12 +1,17 @@
 // application -> index.js
 const explain = require('explain');
 const path = require('path');
+const router = require('./router/router');
+
 exports.main = async (event, context) => {
   return explain.run(event, context, (config) => {
     config.init({
       baseDir: path.resolve(__dirname, 'dist'),
       serviceDir: '/controller/'
     });
+
+    config.route.add(router);
+
     config.filter.add([
       {
         filter: require('./filters/tokenFilter'),
