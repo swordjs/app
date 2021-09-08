b1dbf76096b28d962fc33d95a752dc021962dcfe
seho <1018715564@qq.com>
2021-09-08 23:22:13 +0800

    🐞 fix: 修复已知问题&机器人互动功能

--- uniCloud-aliyun/cloudfunctions/qq-robot/src/services/callback.ts
+++ uniCloud-aliyun/cloudfunctions/qq-robot/src/services/callback.ts
@@ -1,13 +1,38 @@
 const explain = require('explain');
 const createConfig = require('uni-config-center');
+
 const robotConfig = createConfig({
   pluginId: 'qq-robot'
 }) as {
-  config: (string: 'myqq-api') => string;
+  config: () => Record<'myqqApi' | 'myqqRobotUsername' | 'myqqRobotGroup' | 'myqqTriggerMessage', unknown>;
 };
+const Group = require('./group');
+const group = new Group();
+
+interface CallBackBody {
+  MQ_robot: string;
+  MQ_fromQQ: string;
+  MQ_type: string;
+  MQ_msg: string;
+}
 module.exports = class CallBack extends explain.service {
   async main() {
-    uniCloud.logger.log(this);
+    // 只有消息类型为 群 才会触发
+    const { myqqRobotUsername, myqqTriggerMessage } = robotConfig.config();
+    // 判断请求头是否符合标准
+    if ('qqrobot-callback' in this.event.headers && this.event.headers['qqrobot-callback']) {
+      // 判断发送者是否是自身，如果是自身，就不处理
+      const body: CallBackBody = JSON.parse(this.event.body);
+      // 判断机器人qq和参数一样 且 fromqq不是机器人qq
+      if (body.MQ_robot === myqqRobotUsername && body.MQ_fromQQ !== myqqRobotUsername) {
+        // 解析QQ消息
+        const messageData = decodeURI(body.MQ_msg);
+        // 调用发送群组消息的api
+        return await group.postMessage(messageData);
+      } else {
+        return null;
+      }
+    }
   }
 };
 
--- uniCloud-aliyun/cloudfunctions/qq-robot/src/services/group.ts
+++ uniCloud-aliyun/cloudfunctions/qq-robot/src/services/group.ts
@@ -3,25 +3,21 @@ const createConfig = require('uni-config-center');
 const robotConfig = createConfig({
   pluginId: 'qq-robot'
 }) as {
-  config: (string: 'myqq-api') => string;
+  config: () => Record<'myqqApi' | 'myqqRobotUsername' | 'myqqRobotGroup', string>;
 };
 module.exports = class Group extends explain.service {
-  public url: string;
-  constructor() {
-    super();
-    this.url = robotConfig.config('myqq-api');
-  }
-  async postMessage() {
-    const res = await uniCloud.httpclient.request(this.url, {
+  async postMessage(msg: string) {
+    const { myqqApi, myqqRobotUsername, myqqRobotGroup } = robotConfig.config();
+    const res = await uniCloud.httpclient.request(myqqApi, {
       method: 'POST',
       data: {
         function: 'Api_SendMsg', //要调用的函数英文名(查看右侧API列表)
         params: {
-          c1: '3547888277', //参数1，要使用的机器人QQ
+          c1: myqqRobotUsername, //参数1，要使用的机器人QQ
           c2: '2', //参数2，消息类型，2为群，以此类推...
-          c3: '766968150', //参数3，要发送的群号，以此类推...
+          c3: myqqRobotGroup, //参数3，要发送的群号，以此类推...
           c4: '', //参数4，要发送的QQ，此处发的是群，所以这个要留空，以此类推...
-          c5: '前端题目列表: 1. 请写出一个冒泡排序' //参数5，要发送的消息内容，以此类推...
+          c5: msg //参数5，要发送的消息内容，以此类推...
         }
       },
       contentType: 'json',
