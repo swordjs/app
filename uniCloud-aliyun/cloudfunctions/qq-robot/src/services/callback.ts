5d044e01cb4ed8f57675f0aa279fcc17c2277c98
seho <1018715564@qq.com>
2021-09-09 23:14:48 +0800

    ✨ feat: qq机器人自动回复指定语句

--- uniCloud-aliyun/cloudfunctions/qq-robot/src/services/callback.ts
+++ uniCloud-aliyun/cloudfunctions/qq-robot/src/services/callback.ts
@@ -4,7 +4,7 @@ const createConfig = require('uni-config-center');
 const robotConfig = createConfig({
   pluginId: 'qq-robot'
 }) as {
-  config: () => Record<'myqqApi' | 'myqqRobotUsername' | 'myqqRobotGroup' | 'myqqTriggerMessage', unknown>;
+  config: () => Record<'myqqApi' | 'myqqRobotUsername' | 'myqqRobotGroup' | 'myqqTriggerMessage', string | []>;
 };
 const Group = require('./group');
 const group = new Group();
@@ -18,7 +18,7 @@ interface CallBackBody {
 module.exports = class CallBack extends explain.service {
   async main() {
     // 只有消息类型为 群 才会触发
-    const { myqqRobotUsername, myqqTriggerMessage } = robotConfig.config();
+    const { myqqRobotUsername } = robotConfig.config();
     // 判断请求头是否符合标准
     if ('qqrobot-callback' in this.event.headers && this.event.headers['qqrobot-callback']) {
       // 判断发送者是否是自身，如果是自身，就不处理
@@ -27,11 +27,37 @@ module.exports = class CallBack extends explain.service {
       if (body.MQ_robot === myqqRobotUsername && body.MQ_fromQQ !== myqqRobotUsername) {
         // 解析QQ消息
         const messageData = decodeURI(body.MQ_msg);
-        // 调用发送群组消息的api
-        return await group.postMessage(messageData);
+        // 如果消息中含有艾特符号并且艾特是机器人的话，那么就触发
+        const callData = `[%40${myqqRobotUsername}]+`;
+        // 是否艾特了机器人
+        const callSuccessStatus = messageData.indexOf(callData);
+        if (callSuccessStatus > -1) {
+          // 确认艾特了
+          // 调用发送群组消息的api
+          return await group.postMessage((await this.checkTrigger(messageData)) + `[@${body.MQ_fromQQ}]`); // 拼接一个艾特，意指回复
+        }
+        return null;
       } else {
         return null;
       }
+    }
+  }
+  // 判断是否要进行trigger
+  async checkTrigger(message: string) {
+    // 将确认艾特之后才会触发checkTrigger这个方法
+    const { myqqTriggerMessage } = robotConfig.config();
+    if (Array.isArray(myqqTriggerMessage)) {
+      // 循环trigger数组
+      for (const key in myqqTriggerMessage) {
+        const triggerItem = myqqTriggerMessage[key] as { trigger: string[]; message?: string };
+        // 判断当前message是否存在在trigger中 (模糊匹配)
+        for (const triggerIndex in triggerItem.trigger) {
+          if (message.indexOf(triggerItem.trigger[triggerIndex]) > -1) {
+            return triggerItem.message;
+          }
+        }
+      }
+      return '抱歉oh，我没听懂你说的话，请尝试对我说: 群主, 人呢, 有人吗 ';
     }
   }
 };
