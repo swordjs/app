4fb159fedf022c152201e731233d1525260d889c
seho <1018715564@qq.com>
2021-09-11 23:12:06 +0800

    🐞 fix: 完善v1接口工程的filter功能

--- uniCloud-aliyun/cloudfunctions/application/filters/tokenFilter.js
+++ uniCloud-aliyun/cloudfunctions/application/filters/tokenFilter.js
@@ -2,36 +2,25 @@ const explain = require('explain');
 const uniID = require('uni-id');
 module.exports = class tokenFilter extends explain.filter {
   async onActionExecuting() {
-    let { event, context } = this;
-    // 判断context判断请求入口是http还是云函数调用
-    if (context.isHttp) {
-      if (context.args.headers['sword-token'] !== '') {
-        event.uniIdToken = context.args.headers['sword-token'];
-        let params = {};
-        // 判断请求的方法，如果是GET请求那么请求体中会有params嵌套的结构体，需要单独处理
-        if (context.httpMethod === 'GET') {
-          const requestParams = context.args.params;
-          params = JSON.parse(requestParams.params);
-        } else {
-          params = context.args.params;
-        }
-        event.params = params;
-      }
-    }
-    // 判断context中是否存在token
-    if (!event.hasOwnProperty('uniIdToken')) {
-      context.response = {
+    const token_name = 'uni-id-token';
+    let { event, context, explain } = this;
+    // 判断headers中是否存在token
+    if (!event.headers[token_name]) {
+      explain.response.body = {
         code: 401,
         message: 'token不存在'
       };
+      return;
     } else {
       // 检查token合法性和过期时间
-      const checkData = await uniID.getUserInfoByToken(event.uniIdToken);
+      console.log('token是这个：', event.headers[token_name]);
+      const checkData = await uniID.getUserInfoByToken(event.headers[token_name]);
       // token校验不合法
       if (!checkData.uid) {
-        context.response = {
+        explain.response.body = {
           ...checkData
         };
+        return;
       } else {
         context.userID = checkData.uid;
       }
--- uniCloud-aliyun/cloudfunctions/application/index.js
+++ uniCloud-aliyun/cloudfunctions/application/index.js
@@ -3,33 +3,34 @@ const explain = require('explain');
 const path = require('path');
 const router = require('./router/router');
 
-exports.main = async (event, context) => {
-  return explain.run(event, context, (config) => {
-    config.init({
-      baseDir: path.resolve(__dirname, 'dist'),
-      serviceDir: '/controller/'
-    });
-
-    config.route.add(router);
-
-    config.filter.add([
-      {
-        filter: require('./filters/tokenFilter'),
-        ignore: [
-          {
-            service: 'user',
-            actions: ['checkToken', 'loginByWechat', 'getUserContentByID', 'sendSms', 'loginByQQ', 'loginBySms']
-          },
-          {
-            service: 'question',
-            actions: ['addPageView']
-          },
-          {
-            service: 'search',
-            actions: ['addSeachLog']
-          }
-        ]
-      }
-    ]);
+exports.main = async (event, context) =>
+  explain.run({
+    event,
+    context,
+    startup: (app) => {
+      app.init({
+        baseDir: path.resolve(__dirname, 'dist'),
+        serviceDir: '/controller/'
+      });
+      app.filter.add([
+        {
+          filter: require('./filters/tokenFilter'),
+          ignore: [
+            {
+              service: 'user',
+              actions: ['checkToken', 'loginByWechat', 'getUserContentByID', 'sendSms', 'loginByQQ', 'loginBySms']
+            },
+            {
+              service: 'question',
+              actions: ['addPageView']
+            },
+            {
+              service: 'search',
+              actions: ['addSeachLog']
+            }
+          ]
+        }
+      ]);
+      app.route.add(router);
+    }
   });
-};
